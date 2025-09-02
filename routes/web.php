7fbe738b8e5ad94e49868f1c18f873bd9c170b78
aryaw05 <aryawahyuaw77@gmail.com>
2025-09-02 19:42:24 +0700

    feat(routes): add student resource and import routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +13,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    // auth routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // student routes
+    Route::resource('students', StudentController::class);
+
+    Route::post('/students/import' , [StudentController::class, 'import'])->name('students.import');
 });
 
 require __DIR__.'/auth.php';
