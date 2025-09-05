c0bb52e05f55d55196c5d5086e9edac14a33eaae
aryaw05 <aryawahyuaw77@gmail.com>
2025-09-05 17:52:52 +0700

    feat(books): add books management feature with CRUD operations
    - Create Books model with fillable fields
    - Implement BooksController with search functionality
    - Add views for books listing with pagination
    - Include partials for add and update forms

---
+++ app/Http/Controllers/BoooksController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Books;
+use Illuminate\Http\Request;
+
+class BoooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $search  = $request->query('search');
+        
+       $books = Books::when($search, function ($query, $search) {
+            $query->where('title', 'like', "%{$search}%")
+                  ->orWhere('author', 'like', "%{$search}%");
+        })->orderBy('title', 'asc')->paginate(10);
+
+        return view('books.books', compact('books' , 'search'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
