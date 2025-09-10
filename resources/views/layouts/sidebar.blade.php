da430bf4369493b5087b20bcc7da922bccd4b542
aryaw05 <aryawahyuaw77@gmail.com>
2025-09-10 10:53:08 +0700

    fix(sidebar): update books loan link to use proper route

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -117,7 +117,7 @@
                     </li>
                     <li>
                         <a
-                            href="#"
+                            href="{{ route("books-loan.index") }}"
                             class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                         >
                             Data Peminjaman
