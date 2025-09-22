861346be5a461c0f612c5f17cdc56a92c5db5780
aryaw05 <aryawahyuaw77@gmail.com>
2025-09-22 11:22:09 +0700

    feat(students): add phone column and update gender column type

--- resources/views/dashboard/students/students.blade.php
+++ resources/views/dashboard/students/students.blade.php
@@ -68,6 +68,7 @@
                 <th scope="col" class="px-6 py-3">Jenis Kelamin</th>
                 <th scope="col" class="px-6 py-3">Alamat</th>
                 <th scope="col" class="px-6 py-3">Email</th>
+                <th scope="col" class="px-6 py-3">No. Telepon</th>
                 <th scope="col" class="px-6 py-3">Aksi</th>
             </x-slot>
             @forelse ($students as $student)
@@ -107,6 +108,11 @@
                         {{ $student->email ?? "-" }}
                     </td>
 
+                    <!-- No. Telepon -->
+                    <td class="px-6 py-4">
+                        {{ $student->phone ?? "-" }}
+                    </td>
+
                     <!-- Tombol Aksi -->
                     <td class="px-6 py-4 flex gap-4 items-center">
                         <a
@@ -115,7 +121,23 @@
                             aria-hidden="true"
                             href="{{ route("students.edit", $student->id) }}"
                         >
-                            Edit
+                            <svg
+                                class="w-6 h-6 text-gray-800 dark:text-white"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
+                                />
+                            </svg>
                         </a>
 
                         <form
