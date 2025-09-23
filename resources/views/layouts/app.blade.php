9a62ada21eb236088a3ffa079d61743b84d535bb
aryaw05 <aryawahyuaw77@gmail.com>
2025-09-23 10:51:09 +0700

    refactor(views): improve layout structure and add toast notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,47 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config("app.name", "Laravel") }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+            href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
+            rel="stylesheet"
+        />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
     <body class="font-sans antialiased">
-       <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        @if (session("success"))
+            <x-success-toast :message="session('success')" />
+        @endif
 
-                @include('layouts.sidebar')
-                <x-navbar />
-          <main class="p-4 md:ml-64 h-auto pt-20">
+        @if (session("error"))
+            <x-error-toast :message="session('error')" />
+        @endif
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="col-span-full">
+        <div class="antialiased bg-gray-50 dark:bg-gray-900">
+            @include("layouts.sidebar")
+            <x-navbar />
+            <main class="p-4 md:ml-64 h-auto pt-20">
+                <!-- Page Heading -->
+                @isset($header)
+                    <header class="col-span-full">
                         {{ $header }}
-                    </div>
-                </header>
-            @endisset
-            <!-- Main Content -->
-            <main>
-              {{ $slot }}
+                    </header>
+                @endisset
+
+                <!-- Main Content -->
+                <main>
+                    {{ $slot }}
+                </main>
             </main>
-            </div>
-          </main>
-      </div>
+        </div>
     </body>
 </html>
