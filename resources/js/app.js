759b37800b7b560a91ae42bde1afcbec6a8c389d
aryaw05 <aryawahyuaw77@gmail.com>
2025-10-12 20:58:17 +0700

    feat: add TomSelect for book and student search inputs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,29 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
 import "flowbite";
-
+import TomSelect from "tom-select";
+import "tom-select/dist/css/tom-select.css";
 window.Alpine = Alpine;
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.querySelector("#book_id_add")) {
+        new TomSelect("#book_id_add", {
+            create: false,
+            placeholder: "Cari judul buku...",
+            allowEmptyOption: true,
+            sortField: {
+                field: "text",
+                direction: "asc",
+            },
+        });
+    }
+
+    if (document.querySelector("#student_id_add")) {
+        new TomSelect("#student_id_add", {
+            placeholder: "Cari nama siswa...",
+            allowEmptyOption: true,
+        });
+    }
+});
 
 Alpine.start();
