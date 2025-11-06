123403bba38a4f1e565e922cc943b7bd0cd938b7
aryaw05 <aryawahyuaw77@gmail.com>
2025-11-06 08:43:38 +0700

    feat : fix import BookLoan Model

--- app/Exports/booksLoanLateExport.php
+++ app/Exports/booksLoanLateExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\Models\bookLoan;
+use App\Models\BookLoan;
 use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
