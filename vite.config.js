8353024a44fc2f372fdfddc2d19167625dacdde1
aryaw05 <aryawahyuaw77@gmail.com>
2025-11-06 08:12:10 +0700

    fix(vite.config.js): ensure buildDirectory is correctly set for asset management

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 "resources/js/app.js",
                 "resources/js/home.js",
             ],
+            buildDirectory: "build",
             refresh: true,
         }),
     ],
